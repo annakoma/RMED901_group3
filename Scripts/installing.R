968a79bfb22bab6e8261afed0f4db8ca74466c86
annakoma <annakristinaoma@gmail.com>
2023-09-12 09:01:59 +0200

    har flytta alle filene me ikkje treng sånn at me har eit ryddig prosjekt

--- Scripts/installing.R
+++
@@ -1,6 +0,0 @@
-library(usethis)
-use_git_config(user.name = "annakoma", user.email = "annakristinaoma@gmail.com")
-
-# uncomment the line below if you do not have `devtools` installed:
-install.packages("devtools")
-devtools::install_github("jromanowska/BasicGitTutorial")
