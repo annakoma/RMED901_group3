3a4e9e7ba78ad247b66eb6248c250e5bbdd285da
eric-holt <eric.stat.git@gmail.com>
2023-09-12 13:29:14 +0200

    Day 5 and 6 tasks complete (Eric). Changed read_delim(delim = "\t") to read_tsv() for readability.

--- Script/read_tidy.R
+++ Script/read_tidy.R
@@ -1,4 +1,20 @@
 library(tidyverse)
 library(here)
 
-df = read_delim(here("Data", "exam_data.txt"), delim = "\t")
+df_raw = read_tsv(here("Data", "exam_data.txt"))
+
+colnames(df_raw)
+# There's a weird column "wbc_copy" in df
+df_raw[df_raw$wbc != df_raw$wbc_copy, ]
+# and they are identical. 
+
+df_tidy = df_raw %>%
+  distinct %>%
+  setNames(colnames(df_raw) %>% 
+             str_replace_all(" ", "_") %>%
+             str_replace_all("%neut", "neut_percent") %>%
+             str_replace_all("%$", "_percent")) %>%
+  select(-wbc_copy) # Remove the copy.
+# Now 23 columns as intended
+
+write_tsv(df_tidy, here("tidy_data.tsv"))
