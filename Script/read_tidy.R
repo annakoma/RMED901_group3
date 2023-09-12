69054a21bfa4f9abeb8e0e154202e2f0188877f9
eric-holt <eric.stat.git@gmail.com>
2023-09-12 13:29:14 +0200

    Day 5 tasks

--- Script/read_tidy.R
+++ Script/read_tidy.R
@@ -2,3 +2,12 @@ library(tidyverse)
 library(here)
 
 df = read_delim(here("Data", "exam_data.txt"), delim = "\t")
+colnames(df)
+df = df %>%
+  distinct %>%
+  setNames(colnames(df) %>% 
+             str_replace_all(" ", "_") %>%
+             str_replace_all("^%", "percent_") %>%
+             str_replace_all("%$", "_percent"))
+
+write_delim(df, here("tidy_data.tsv"))
