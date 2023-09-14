080d087f67eff79293c0edef79461ff627914b56
eric-holt <eric.stat.git@gmail.com>
2023-09-14 10:00:30 +0200

    Refactor Day 5 and 6

--- Script/adjust_explore.R
+++ Script/adjust_explore.R
@@ -2,34 +2,28 @@ library(tidyverse)
 library(here)
 
 # Preparation----
-df = read_table(here("tidy_data.tsv"))
-df_to_join = read_table(here("Data", "exam_data_joindata.txt"))
+df_tidy = read_table(here("tidy_data.tsv"))
+df_additional = read_table(here("Data", "exam_data_joindata.txt"))
 
-colnames(df)
-colnames(df_to_join)
-colnames(df)[colnames(df) %in% colnames(df_to_join)]
-colnames(df_to_join)[colnames(df_to_join) %in% colnames(df)]
+colnames(df_tidy)
+colnames(df_additional)
+colnames(df_tidy)[colnames(df_tidy) %in% colnames(df_additional)]
+colnames(df_additional)[colnames(df_additional) %in% colnames(df_tidy)]
 # Only the patient_id is common.
-
-# There's a weird column "wbc_copy" in df
-df[df$wbc != df$wbc_copy, ]
-# and they are identical. Removing the copy.
-df = select(df, -wbc_copy)
-# Now 23 columns as intended
 
 # There's an extra "gender" column in the additional data, but that's probably fine
 
 
 # Tasks----
-identical(unique(df$patient_id), unique(df_to_join$patient_id))
+identical(unique(df_tidy$patient_id), unique(df_additional$patient_id))
 # All patients IDs are present in both data; it doesn't matter how to join them
 
 # Remove hct and rdw, then join by the patient ID
-df_joined = df %>% 
-  select(-c("hct", "rdw")) %>% left_join(df_to_join, by = "patient_id")
+df_joined = df_tidy %>% 
+  select(-c("hct", "rdw")) %>% left_join(df_additional, by = "patient_id")
 
 # Change data types, create new columns, set column order, sort by patient ID
-df_joined %>%
+df_adjuSted = df_joined %>%
   mutate(across(c(active, remission), as.logical)) %>%
   mutate(across(c(mean_RBC_characteristic, gender), as.factor)) %>%
   mutate(lymph_count = wbc * lymph_percent,
@@ -40,10 +34,10 @@ df_joined %>%
   arrange(patient_id)
 
 # Write for examination (maybe in Excel)
-write_tsv(df_joined, here("joined_adjusted.tsv"))
+write_tsv(df_adjuSted, here("joined_adjusted.tsv"))
 
 # glucose by gender
-df_joined %>%
+df_adjuSted %>%
   group_by(gender) %>%
   summarize(
     min_gluc = min(gluc, na.rm = T),
@@ -53,7 +47,7 @@ df_joined %>%
   )
 
 # glucose by gender with hgb <= 10
-df_joined %>%
+df_adjuSted %>%
   filter(hgb <= 10) %>%
   group_by(gender) %>%
   summarize(
@@ -64,7 +58,7 @@ df_joined %>%
   )
 
 # glucose by gender with remission
-df_joined %>%
+df_adjuSted %>%
   filter(remission == T) %>%
   group_by(gender) %>%
   summarize(
@@ -75,7 +69,7 @@ df_joined %>%
   )
 
 # glucose by gender for older than around 40 years
-df_joined %>%
+df_adjuSted %>%
   filter(days_of_life > 40 * 365.25) %>%
   group_by(gender) %>%
   summarize(
@@ -86,7 +80,7 @@ df_joined %>%
   )
 
 # glucose by gender with more than 10% of monocytes in WBC
-df_joined %>%
+df_adjuSted %>%
   filter(mono_percent > 10) %>%
   group_by(gender) %>%
   summarize(
@@ -97,5 +91,5 @@ df_joined %>%
   )
 
 # table by gender and remission
-df_joined %>%
+df_adjuSted %>%
   count(gender, remission)
--- Script/read_tidy.R
+++ Script/read_tidy.R
@@ -4,13 +4,17 @@ library(here)
 df_raw = read_tsv(here("Data", "exam_data.txt"))
 
 colnames(df_raw)
+# There's a weird column "wbc_copy" in df
+df_raw[df_raw$wbc != df_raw$wbc_copy, ]
+# and they are identical. 
 
-df_col_fixed = df_raw %>%
+df_tidy = df_raw %>%
   distinct %>%
   setNames(colnames(df_raw) %>% 
              str_replace_all(" ", "_") %>%
              str_replace_all("%neut", "neut_percent") %>%
              str_replace_all("%$", "_percent")) %>%
-  select(-wbc_copy)
+  select(-wbc_copy) # Remove the copy.
+# Now 23 columns as intended
 
-write_tsv(df_col_fixed, here("tidy_data.tsv"))
+write_tsv(df_tidy, here("tidy_data.tsv"))
